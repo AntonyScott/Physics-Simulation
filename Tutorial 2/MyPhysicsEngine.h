e6976e73ffd7ace9ee86019e73519b63eab50946
Antony Scott <antonyscott1@hotmail.co.uk>
2023-03-29 15:09:45 +0100

    Commit 15
    Added trampoline and joint classes. Joint now working although it isn't working well. Will fix later on

--- Tutorial 2/MyPhysicsEngine.h
+++ Tutorial 2/MyPhysicsEngine.h
@@ -12,6 +12,176 @@ namespace PhysicsEngine
 	static const PxVec3 color_palette[] = { PxVec3(46.f / 255.f,9.f / 255.f,39.f / 255.f),PxVec3(217.f / 255.f,0.f / 255.f,0.f / 255.f),
 		PxVec3(255.f / 255.f,45.f / 255.f,0.f / 255.f),PxVec3(255.f / 255.f,140.f / 255.f,54.f / 255.f),PxVec3(4.f / 255.f,117.f / 255.f,111.f / 255.f) };
 
+	//pyramid vertices
+	static PxVec3 pyramid_verts[] = { PxVec3(0,1,0), PxVec3(1,0,0), PxVec3(-1,0,0), PxVec3(0,0,1), PxVec3(0,0,-1) };
+	//pyramid triangles: a list of three vertices for each triangle e.g. the first triangle consists of vertices 1, 4 and 0
+	//vertices have to be specified in a counter-clockwise order to assure the correct shading in rendering
+	static PxU32 pyramid_trigs[] = { 1, 4, 0, 3, 1, 0, 2, 3, 0, 4, 2, 0, 3, 2, 1, 2, 4, 1 };
+
+	class Pyramid : public ConvexMesh
+	{
+	public:
+		Pyramid(PxTransform pose = PxTransform(PxIdentity), PxReal density = 1.f) :
+			ConvexMesh(vector<PxVec3>(begin(pyramid_verts), end(pyramid_verts)), pose, density)
+		{
+			//CreateShape(PxPlaneGeometry(pose, density));
+		}
+	};
+
+	class PyramidStatic : public TriangleMesh
+	{
+	public:
+		PyramidStatic(PxTransform pose = PxTransform(PxIdentity)) :
+			TriangleMesh(vector<PxVec3>(begin(pyramid_verts), end(pyramid_verts)), vector<PxU32>(begin(pyramid_trigs), end(pyramid_trigs)), pose)
+		{
+		}
+	};
+
+	struct FilterGroup
+	{
+		enum Enum
+		{
+			ACTOR0 = (1 << 0),
+			ACTOR1 = (1 << 1),
+			ACTOR2 = (1 << 2)
+			//add more if you need
+		};
+	};
+
+	///An example class showing the use of springs (distance joints).
+	class Trampoline
+	{
+		vector<DistanceJoint*> springs;
+		Box* bottom, * top;
+
+	public:
+		Trampoline(const PxVec3& dimensions = PxVec3(1.f, 1.f, 1.f), PxReal stiffness = 1.f, PxReal damping = 1.f)
+		{
+			PxReal thickness = .1f;
+			bottom = new Box(PxTransform(PxVec3(0.f, thickness, 0.f)), PxVec3(dimensions.x, thickness, dimensions.z));
+			top = new Box(PxTransform(PxVec3(0.f, dimensions.y + thickness, 0.f)), PxVec3(dimensions.x, thickness, dimensions.z));
+			springs.resize(4);
+			springs[0] = new DistanceJoint(bottom, PxTransform(PxVec3(dimensions.x, thickness, dimensions.z)), top, PxTransform(PxVec3(dimensions.x, -dimensions.y, dimensions.z)));
+			springs[1] = new DistanceJoint(bottom, PxTransform(PxVec3(dimensions.x, thickness, -dimensions.z)), top, PxTransform(PxVec3(dimensions.x, -dimensions.y, -dimensions.z)));
+			springs[2] = new DistanceJoint(bottom, PxTransform(PxVec3(-dimensions.x, thickness, dimensions.z)), top, PxTransform(PxVec3(-dimensions.x, -dimensions.y, dimensions.z)));
+			springs[3] = new DistanceJoint(bottom, PxTransform(PxVec3(-dimensions.x, thickness, -dimensions.z)), top, PxTransform(PxVec3(-dimensions.x, -dimensions.y, -dimensions.z)));
+
+			for (unsigned int i = 0; i < springs.size(); i++)
+			{
+				springs[i]->Stiffness(stiffness);
+				springs[i]->Damping(damping);
+			}
+		}
+
+		void AddToScene(Scene* scene)
+		{
+			scene->Add(bottom);
+			scene->Add(top);
+		}
+
+		~Trampoline()
+		{
+			for (unsigned int i = 0; i < springs.size(); i++)
+				delete springs[i];
+		}
+	};
+
+	///A customised collision class, implemneting various callbacks
+	class MySimulationEventCallback : public PxSimulationEventCallback
+	{
+	public:
+		//an example variable that will be checked in the main simulation loop
+		bool trigger;
+
+		MySimulationEventCallback() : trigger(false) {}
+
+		///Method called when the contact with the trigger object is detected.
+		virtual void onTrigger(PxTriggerPair* pairs, PxU32 count)
+		{
+			//you can read the trigger information here
+			for (PxU32 i = 0; i < count; i++)
+			{
+				//filter out contact with the planes
+				if (pairs[i].otherShape->getGeometryType() != PxGeometryType::ePLANE)
+				{
+					//check if eNOTIFY_TOUCH_FOUND trigger
+					if (pairs[i].status & PxPairFlag::eNOTIFY_TOUCH_FOUND)
+					{
+						cerr << "onTrigger::eNOTIFY_TOUCH_FOUND" << endl;
+						trigger = true;
+					}
+					//check if eNOTIFY_TOUCH_LOST trigger
+					if (pairs[i].status & PxPairFlag::eNOTIFY_TOUCH_LOST)
+					{
+						cerr << "onTrigger::eNOTIFY_TOUCH_LOST" << endl;
+						trigger = false;
+					}
+				}
+			}
+		}
+
+		///Method called when the contact by the filter shader is detected.
+		virtual void onContact(const PxContactPairHeader& pairHeader, const PxContactPair* pairs, PxU32 nbPairs)
+		{
+			cerr << "Contact found between " << pairHeader.actors[0]->getName() << " " << pairHeader.actors[1]->getName() << endl;
+
+			//check all pairs
+			for (PxU32 i = 0; i < nbPairs; i++)
+			{
+				//check eNOTIFY_TOUCH_FOUND
+				if (pairs[i].events & PxPairFlag::eNOTIFY_TOUCH_FOUND)
+				{
+					cerr << "onContact::eNOTIFY_TOUCH_FOUND" << endl;
+				}
+				//check eNOTIFY_TOUCH_LOST
+				if (pairs[i].events & PxPairFlag::eNOTIFY_TOUCH_LOST)
+				{
+					cerr << "onContact::eNOTIFY_TOUCH_LOST" << endl;
+				}
+			}
+		}
+
+		virtual void onConstraintBreak(PxConstraintInfo* constraints, PxU32 count) {}
+		virtual void onWake(PxActor** actors, PxU32 count) {}
+		virtual void onSleep(PxActor** actors, PxU32 count) {}
+#if PX_PHYSICS_VERSION >= 0x304000
+		virtual void onAdvance(const PxRigidBody* const* bodyBuffer, const PxTransform* poseBuffer, const PxU32 count) {}
+#endif
+	};
+
+	//A simple filter shader based on PxDefaultSimulationFilterShader - without group filtering
+	static PxFilterFlags CustomFilterShader(PxFilterObjectAttributes attributes0, PxFilterData filterData0,
+		PxFilterObjectAttributes attributes1, PxFilterData filterData1,
+		PxPairFlags& pairFlags, const void* constantBlock, PxU32 constantBlockSize)
+	{
+		// let triggers through
+		if (PxFilterObjectIsTrigger(attributes0) || PxFilterObjectIsTrigger(attributes1))
+		{
+			pairFlags = PxPairFlag::eTRIGGER_DEFAULT;
+			return PxFilterFlags();
+		}
+
+		pairFlags = PxPairFlag::eCONTACT_DEFAULT;
+		//enable continous collision detection
+//		pairFlags |= PxPairFlag::eCCD_LINEAR;
+
+
+		//customise collision filtering here
+		//e.g.
+
+		// trigger the contact callback for pairs (A,B) where 
+		// the filtermask of A contains the ID of B and vice versa.
+		if ((filterData0.word0 & filterData1.word1) && (filterData1.word0 & filterData0.word1))
+		{
+			//trigger onContact callback for this pair of objects
+			pairFlags |= PxPairFlag::eNOTIFY_TOUCH_FOUND;
+			pairFlags |= PxPairFlag::eNOTIFY_TOUCH_LOST;
+			//			pairFlags |= PxPairFlag::eNOTIFY_CONTACT_POINTS;
+		}
+
+		return PxFilterFlags();
+	};
+
 	///Custom scene class
 	class MyScene : public Scene
 	{
@@ -28,7 +198,8 @@ namespace PhysicsEngine
 		InnerBarrierLines* innerBarrierLines;
 		OuterBarrierLines* outerBarrierLines;
 		BallCatapult* ballCatapult;
-		//RevoluteJoint brickChain(swingTopBar, (PxTransform(PxVec3(0.f, 2.f, -38.85714287f)), ));
+		Trampoline* trampoline;
+		RevoluteJoint* ballChain;
 
 		PxMaterial* rubberMat = CreateMaterial(0.9f, 0.65f, 0.828f);
 		//https://saferroadsconference.com/wp-content/uploads/2016/05/Peter-Cenek-Frictional-Characteristics-Roadside-Grass-Types.pdf
@@ -71,6 +242,12 @@ namespace PhysicsEngine
 			//brick spawn function
 			Brick();
 
+			//Trampoline::AddToScene()
+			/*trampoline = new Trampoline(PxVec3(3.f, 3.f, 3.f), PxReal(5.f), PxReal(5.f));
+			trampoline->AddToScene(this);*/
+
+			ballChain = new RevoluteJoint(swingTopBar, PxTransform(PxVec3(2.f, 0.f, 0.f)), brick, PxTransform(PxVec3(0.f, 5.f, 0.f)));
+			//Add(ballChain);
 			//catapult spawn function
 			//Catapult();
 
@@ -138,7 +315,6 @@ namespace PhysicsEngine
 
 			swingTopBar = new SwingTopBar(PxTransform(PxVec3(0.f, 0.f, -42.85714287f)));
 			Add(swingTopBar);
-
 		}
 
 		void Ball() 
@@ -158,7 +334,7 @@ namespace PhysicsEngine
 			Add(ballCatapult);
 		}
 
-		//Custom udpate function
+		//Custom update function
 		virtual void CustomUpdate()
 		{
 
